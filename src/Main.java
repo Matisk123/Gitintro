15313a69a17cf296a4c67f9979d1e7e8797f6d22
Matisk123 <matis.kohn@edu.esiee-it.fr>
2024-10-07 11:58:14 +0200

    Add git log

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
+git add
+        public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello world!");*
+        System.out.println("Modif Gif");
     }
 }
